a834786878326431b309963e2f9393cb440b910d
kakedashi3 <riki_funk_x@yahoo.co.jp>
2023-09-01 15:43:59 +0900

    first commit

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -1,34 +1,9 @@
-// pages/api/metadata/[id].js
+import data from './data/data.json';
 
 export default (req, res) => {
     const {
       query: { id },
     } = req;
-  
-    // 仮のデータベース（実際のアプリケーションではデータベースからデータを取得します）
-    const data = {
-      1: { 
-            tokenId: 1,
-            name: "kakedashi", 
-            description: "This is NFT 1" ,
-            image:"",
-            external_url:"",
-            attributes: [
-                {
-                trait_type: "",
-                value:""
-                },
-            ]
-
-        },
-
-
-      2: { 
-            name: "NFT 2", 
-            description: "This is NFT 2" 
-        },
-      // ... 他のデータも追加可能
-    };
   
     const nftData = data[id];
   
@@ -37,5 +12,4 @@ export default (req, res) => {
     } else {
       res.status(404).json({ error: "NFT not found" });
     }
-  };
-  
+};
